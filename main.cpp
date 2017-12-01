a674021a0f033e011f8b00e443e0afe8d59ec338
comwrg <xcomwrg@gmail.com>
2017-12-01 13:43:33 +0800

    finish initially

--- main.cpp
+++ main.cpp
@@ -65,7 +65,78 @@ using namespace std;
 0 0 0 0 0 0 0 0 0 2
 66
  */
+static vector<vector<int>> arr(10);
 
+bool add(const vector<int> & v1, const vector<int> & v2, vector<int> & v3) {
+    auto size = v1.size();
+    int r = 0;
+    for (int i = 0; i < size; i++) {
+        int sum = v1[i] + v2[i] + r;
+        v3[i] = sum % 10;
+        r = sum / 10;
+    }
+    return r == 0;
+}
+
+bool multiply(vector<int> & v, int n) {
+    int r = 0;
+    for (int & i : v) {
+        int sum = i * n;
+        sum += r;
+        i = sum % 10;
+        r = sum / 10;
+    }
+    return r == 0;
+}
+
+void init_arr() {
+    for (int i = 0; i < 10; i++) {
+        auto& v = arr[i];
+        v = vector<int>(21);
+        v[0] = 1;
+        for (int j = 0; j < 21; j++) {
+            multiply(v, i);
+        }
+
+        // print
+        for (int j = 0; j < 21; j++) {
+            cout << v[j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "----" << endl;
+}
+
+void handle(const vector<int> & vec) {
+    // vec len is 10
+    // 21 0 0 0
+    // 21 * 0**21
+    auto size = vec.size();
+    vector<int> sum(21);
+    for (int i = 0; i < size; i++) {
+        vector<int> v(arr[i]);
+        bool b;
+        b = multiply(v, vec[i]);
+        if (!b) return;
+        b = add(v, sum, sum);
+        if (!b) return;
+    }
+    if (sum[20] == 0) return;
+    vector<int> counter(10);
+    for (int i = 0; i < 21; i++) {
+        counter[sum[i]]++;
+    }
+    for (int i = 0; i < 10; i++) {
+        if (counter[i] != vec[i])
+            return;
+    }
+
+    for (int i = 0; i < 21; i++) {
+        cout << sum[i] << " ";
+    }
+    cout << endl;
+
+}
 
 void fun1(vector<int> vec, int & cnt) {
     cnt++;
@@ -112,11 +183,13 @@ void fun2(vector<int> vec, int sum, int left, int & cnt) {
 
     if (pos == vec.size() - 1) {
         cnt++;
-//        vec[pos] = left;
+        vec[pos] = left;
 //        for (int i : vec) {
 //            cout << i << " ";
 //        }
 //        cout << endl;
+
+        handle(vec);
         return;
     }
 
@@ -145,6 +218,7 @@ void fun2(vector<int> vec, int sum, int left, int & cnt) {
 
 int main() {
     auto time_begin = clock();
+    init_arr();
     vector<int> vec(10);
     for (int &i : vec) {
         i = -1;
